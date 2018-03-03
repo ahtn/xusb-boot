4aea8332447e57fac91bd44fd8038c1aed997474
ahtn <jem@seethis.link>
2018-03-03 12:17:35 +1000

    update the python package for the bootloader

--- scripts/setup.py
+++ scripts/setup.py
@@ -4,17 +4,28 @@
 # Licensed under the MIT license (http://opensource.org/licenses/MIT)
 
 from setuptools import setup
+import os
+
+# Load the version number
+try: # python3
+    fields = {}
+    with open(os.path.join("xusbboot", "version.py")) as f:
+        exec(f.read(), fields)
+    __version__ = fields['__version__']
+except: # python2
+    execfile(os.path.join("xusbboot", "version.py"))
 
 setup(
-    name = 'xusb',
-    version = '0.0.0',
-    description = "Python library for Xmega xusb bootloader",
+    name = 'xusbboot',
+    version = '0.0.1',
+    description = "Python library for xmega xusb bootloader",
     url = "http://github.com/ahtn/xusb-boot",
     author = "jem",
     author_email = "jem@seethis.link",
     license = 'MIT',
-    packages = ['xusb'],
+    packages = ['xusbboot'],
     install_requires = ['hexdump', 'intelhex', 'easyhid'],
     keywords = ['xmega', 'usb', 'hid', 'avr', 'bootloader'],
+    scripts = ['bin/xusbboot-cli'],
     zip_safe = False
 )
